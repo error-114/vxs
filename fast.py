f2d2a1cdb6ef8a7551c8f8aff8e1c9d42936f54c
Error-114 <82705431+error-114@users.noreply.github.com>
2021-04-19 05:12:21 +0300

    Update fast.py

--- fast.py
+++ fast.py
@@ -1,8 +1,8 @@
 #iqm4ra < / >
 import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
 os.system('rm -rf .txt')
-for n in range(12121):
-    nmbr = random.randint(1010101, 9999999)
+for n in range(18000):
+    nmbr = random.randint(0000000, 9999999)
     sys.stdout = open('.txt', 'a')
     print nmbr
     sys.stdout.flush()
